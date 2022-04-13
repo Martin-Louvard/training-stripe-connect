415b3844676e01d23d81f52507812aa9f414f533
Martin <martin.louvard@gmail.com>
2022-04-13 18:07:05 +0200

    Stripe implemented with connected accounts

--- app/controllers/stripe_controller.rb
+++ app/controllers/stripe_controller.rb
@@ -27,8 +27,7 @@ class StripeController < ApplicationController
     def create_checkout
         @user = current_user
         @product = Product.find(params[:product_id])
-        @session = Stripe::Checkout::Session.create(
-          payment_method_types: ['card'],
+        @session = Stripe::Checkout::Session.create({
             line_items: [{
               price_data: {
                 currency: 'usd',
@@ -39,10 +38,16 @@ class StripeController < ApplicationController
               },
               quantity: 1,
             }],
+            payment_intent_data: {
+              application_fee_amount: 123,
+              transfer_data: {
+                destination: @product.seller.stripe_user_id,
+              },
+            },
             mode: 'payment',
             success_url: checkout_success_url + '?session_id={CHECKOUT_SESSION_ID}',
             cancel_url: checkout_cancel_url,
-        )
+          })
 
         redirect_to @session.url, status:303, allow_other_host: true
       end
